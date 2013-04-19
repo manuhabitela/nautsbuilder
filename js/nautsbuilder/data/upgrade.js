1d70e450fc1fc909212fffe4432c9558c3165ab4
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-04-19 22:21:39 +0200

    showing current and max steps of an upgrade

--- js/nautsbuilder/data/upgrade.js
+++ js/nautsbuilder/data/upgrade.js
@@ -1,5 +1,19 @@
 leiminauts.Upgrade = Backbone.Model.extend({
+	defaults: {
+		current_step: 0,
+		max_step: 0
+	},
 
+	initialize: function(attrs, opts) {
+		this._setMaxStep();
+	},
+
+	_setMaxStep: function() {
+		var steps = _(this.attributes).filter(function(attr, key) {
+			return (/^step[0-9]$/).test(key) && attr !== "";
+		});
+		this.set('max_step', steps.length);
+	}
 });
 
 leiminauts.Upgrades = Backbone.Collection.extend({
