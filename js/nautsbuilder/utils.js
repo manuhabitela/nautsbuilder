81e0c7f84f572bcc22a8af861081d75e79927d29
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-04-27 01:19:23 +0200

    each step has its attributes list built at initialization

--- js/nautsbuilder/utils.js
+++ js/nautsbuilder/utils.js
@@ -3,6 +3,18 @@ _.mixin({
 	underscored : function(string) {
 		if (!_.isString(string)) return false;
 		return string.toLowerCase().replace(' ', '_');
+	},
+	//http://stackoverflow.com/questions/3000649/trim-spaces-from-start-and-end-of-string
+	//https://github.com/epeli/underscore.string/blob/master/lib/underscore.string.js#L346
+	trim: function(string, characters) {
+		if (!string) return '';
+		characters = characters || null;
+		if (typeof String.prototype.trim != 'function' || characters) {
+			var pattern = characters ? characters : '\\s';
+			return String(string).replace(new RegExp('^' + pattern + '+|' + pattern + '+$', 'g'), '');
+		} else {
+			return string.trim();
+		}
 	}
 });
 
