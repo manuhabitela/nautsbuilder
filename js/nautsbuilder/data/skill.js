ab452df894d575f13e26c8ff083974ffd269fa66
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-05-01 19:05:20 +0200

    Mozilla Public License v2.0

--- js/nautsbuilder/data/skill.js
+++ js/nautsbuilder/data/skill.js
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * copyright (c) 2013, Emmanuel Pelletier
+ */
 leiminauts.Skill = Backbone.Model.extend({
 	initialize: function(attrs, opts) {
 		this.set('upgrades', new leiminauts.Upgrades());
