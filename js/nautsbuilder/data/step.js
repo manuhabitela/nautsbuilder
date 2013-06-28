4f16d8a12e8460961ffe63b0331229f57f3f281b
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-06-28 19:53:08 +0200

    not showing @ flag in description

--- js/nautsbuilder/data/step.js
+++ js/nautsbuilder/data/step.js
@@ -10,10 +10,16 @@ leiminauts.Step = Backbone.Model.extend({
 	},
 
 	initialize: function() {
-		this.set('attrs', leiminauts.utils.treatEffects(this.get('description')));
 		//description is a string showing the list of effects the step gives. Ex: "crit chance: +15%; crit damage: +10"
 		//so each attribute is separated by a ";"
 		//and attribute name and value are separated by a ":"
+		this.set('attrs', leiminauts.utils.treatEffects(this.get('description')));
+
+		this.updateDescription();
+	},
+
+	updateDescription: function() {
+		this.set('description', this.get('description').replace(': @', ': '));
 	}
 });
 
