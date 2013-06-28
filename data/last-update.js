9c47ee3fab1174efae900462918f26affc3fa3ab
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-06-28 19:22:40 +0200

    adding server data update php script this is called from client side to update all the json contained in the data folder
    last-update.js contains the timestamp of the update

---
+++ data/last-update.js
@@ -0,0 +1,3 @@
+window.leiminauts = window.leiminauts || {};
+
+leiminauts.lastServerDataUpdate = 1372439795000; //2013-06-28 19:16:35
---
+++ data/update.php
@@ -0,0 +1,31 @@
+<?php
+if (!empty($_POST)) {
+	$sheet = !empty($_POST['sheet']) ? $_POST['sheet'] : '';
+	if (empty($sheet))
+		return false;
+
+	$characters = !empty($_POST['characters']) ? $_POST['characters'] : '';
+	$skills = !empty($_POST['skills']) ? $_POST['skills'] : '';
+	$upgrades = !empty($_POST['upgrades']) ? $_POST['upgrades'] : '';
+	$data = array('characters' => $characters, 'skills' => $skills, 'upgrades' => $upgrades);
+
+	$errors = 0;
+
+	$lastUpdate = "window.leiminauts = window.leiminauts || {};\n
+leiminauts.lastServerDataUpdate = " . time()*1000 . "; //" . date('Y-m-d H:i:s');
+
+	foreach ($data as $type => $json) {
+		$testJson = json_decode($json);
+		if (json_last_error() == JSON_ERROR_NONE) {
+			file_put_contents(dirname(__FILE__).'/'.$sheet.'-'.$type.'.json', $json);
+			echo "\n", $sheet, ' ', $type, " ok";
+		} else {
+			$errors++;
+			echo "\n", $sheet, ' ', $type, " NOT OK MAN, NOT OKAY, OKAY?";
+		}
+	}
+
+	if ($errors === 0 || $errors !== count($data)) {
+		file_put_contents(dirname(__FILE__).'/last-update.js', $lastUpdate);
+	}
+}
