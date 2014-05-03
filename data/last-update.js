b48dcb837b4ff87003897d9215ae25c4aca38307
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2014-05-03 18:26:16 +0200

    v0.13 data

--- data/last-update.js
+++ data/last-update.js
@@ -1,3 +1,3 @@
 window.leiminauts = window.leiminauts || {};
 
-leiminauts.lastServerDataUpdate = 1395597010000; //2014-03-23 18:50:10
+leiminauts.lastServerDataUpdate = 1399134332000; //2014-05-03 18:25:32
--- js/nautsbuilder/spreadsheet/last-update.js
+++ js/nautsbuilder/spreadsheet/last-update.js
@@ -1,3 +1,3 @@
 window.leiminauts = window.leiminauts || {};
 
-leiminauts.lastSpreadsheetUpdate = 1395597008000; //2014-03-23 18:50:08
+leiminauts.lastSpreadsheetUpdate = 1399134330000; //2014-05-03 18:25:30
