91e054c0246dbcae2fd33a881165b0579d9d5c36
unknown <k.rafftry@gmail.com>
2015-08-23 19:47:41 +0100

    Fixed Beta character issue, transparency on src images adapted for, minor optimisations to when smaller viewport is seen

--- data/last-update.js
+++ data/last-update.js
@@ -1,3 +1,3 @@
 window.leiminauts = window.leiminauts || {};
 
-leiminauts.lastServerDataUpdate = 1438883832000; //2015-08-06 17:57:12
+leiminauts.lastServerDataUpdate = 1440348564000; //2015-08-23 16:49:24
