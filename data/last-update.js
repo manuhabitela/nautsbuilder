0528b5f0a8ff2fe4c151ef9756d0755484388a33
r0estir0bbe <r0estir0bbe@gmx.com>
2016-03-20 20:44:00 +0100

    Removed all generated files from the repository.

--- data/last-update.js
+++
@@ -1,3 +0,0 @@
-window.leiminauts = window.leiminauts || {};
-
-leiminauts.lastServerDataUpdate = 1441031416000; //2015-08-31 16:30:16
--- js/nautsbuilder/spreadsheet/last-update.js
+++
@@ -1,3 +0,0 @@
-window.leiminauts = window.leiminauts || {};
-
-leiminauts.lastSpreadsheetUpdate = 1441031414000; //2015-08-31 16:30:14
